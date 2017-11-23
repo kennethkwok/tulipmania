75e63810b45f6c4723b1b935fdc60f223865409f
Ken Kwok <kwok.kenneth@gmail.com>
2017-11-23 22:08:59 +0000

    remove redundant object block from CurrencyUtils #mobile

--- mobile/src/main/java/co/uk/kenkwok/tulipmania/utils/CurrencyUtils.kt
+++ mobile/src/main/java/co/uk/kenkwok/tulipmania/utils/CurrencyUtils.kt
@@ -5,30 +5,28 @@ import java.text.DecimalFormat
 /**
  * Created by kwokk on 17/11/2017.
  */
-class CurrencyUtils {
-    companion object {
-        private val df = DecimalFormat("###,###.00")
+object CurrencyUtils {
+    private val df = DecimalFormat("###,###.00")
 
-        /**
-         * Converts amount returned by ticker API to preferred DecimalFormat string.
-         * Currently only supports USD or HKD.
-         */
-        fun convertDisplayCurrency(displayCurrency: String) : String {
-            if (displayCurrency.endsWith(" usd", true) ||
-                    displayCurrency.endsWith(" hkd", true)) {
-                val strList = displayCurrency.split(" ")
-                return formatSeparatorsAndDecimals(strList[0])
-            } else {
-                return formatSeparatorsAndDecimals(displayCurrency)
-            }
+    /**
+     * Converts amount returned by ticker API to preferred DecimalFormat string.
+     * Currently only supports USD or HKD.
+     */
+    fun convertDisplayCurrency(displayCurrency: String) : String {
+        if (displayCurrency.endsWith(" usd", true) ||
+                displayCurrency.endsWith(" hkd", true)) {
+            val strList = displayCurrency.split(" ")
+            return formatSeparatorsAndDecimals(strList[0])
+        } else {
+            return formatSeparatorsAndDecimals(displayCurrency)
         }
+    }
 
-        /**
-         * Converts a string numerical amount such as 1234 to 1,234.00
-         */
-        private fun formatSeparatorsAndDecimals(amount: String): String {
-            var amount = amount.replace(",", "")
-            return "$".plus(df.format(amount.toDouble()))
-        }
+    /**
+     * Converts a string numerical amount such as 1234 to 1,234.00
+     */
+    private fun formatSeparatorsAndDecimals(amount: String): String {
+        var amount = amount.replace(",", "")
+        return "$".plus(df.format(amount.toDouble()))
     }
 }

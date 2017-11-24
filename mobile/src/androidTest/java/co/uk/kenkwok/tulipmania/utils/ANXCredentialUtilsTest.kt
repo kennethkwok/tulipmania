af453c40ea90f3caf6abdb4f9c6f7bf24ec3add9
Ken Kwok <kwok.kenneth@gmail.com>
2017-11-24 10:23:54 +0000

    add instrumentation tests for ANX creds and NetworkUtils #mobile

---
+++ mobile/src/androidTest/java/co/uk/kenkwok/tulipmania/utils/ANXCredentialUtilsTest.kt
@@ -0,0 +1,27 @@
+package co.uk.kenkwok.tulipmania.utils
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by kwokk on 23/11/2017.
+ */
+@RunWith(AndroidJUnit4::class)
+class ANXCredentialUtilsTest {
+    private val context: Context = InstrumentationRegistry.getTargetContext()
+
+    @Test
+    fun getANXCredentialsTest() {
+        val credentials = ANXCredentialUtils.getApiCredentials(context)
+        assertNotNull(credentials.apiKey)
+        assertNotNull(credentials.apiSecret)
+
+        assertTrue(credentials.apiKey, credentials.apiKey.startsWith("edbda4a7"))
+        assertTrue(credentials.apiSecret, credentials.apiSecret.startsWith("ZKIe6CT"))
+    }
+}
---
+++ mobile/src/androidTest/java/co/uk/kenkwok/tulipmania/utils/NetworkUtilsTest.kt
@@ -0,0 +1,26 @@
+package co.uk.kenkwok.tulipmania.utils
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by kwokk on 24/11/2017.
+ */
+@RunWith(AndroidJUnit4::class)
+class NetworkUtilsTest {
+    private val context: Context = InstrumentationRegistry.getTargetContext()
+
+    @Test
+    fun generateRestSignTest() {
+        val credentials = ANXCredentialUtils.getApiCredentials(context)
+        val data = "/btcusd/money/ticker"
+        val restSign = NetworkUtils.generateRestSign(credentials.apiSecret, data.toByteArray())
+
+        Assert.assertNotNull(restSign)
+        Assert.assertTrue(restSign.isNotEmpty())
+    }
+}
